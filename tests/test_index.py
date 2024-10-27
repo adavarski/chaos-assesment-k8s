b42f4afd3740604241e5b137d9bddddbd5cf6f0d
A.Davarski <42774466+adavarski@users.noreply.github.com>
2024-10-27 14:44:01 +0200

    Delete tests/test_index.py

--- tests/test_index.py
+++
@@ -1,6 +0,0 @@
-def test_index(client):
-    response = client.get("/greet")
-    assert response.status_code == 200
-
-    expected = "<p>Now everyone can be a hero...</p>"
-    assert expected == response.get_data(as_text=True)
