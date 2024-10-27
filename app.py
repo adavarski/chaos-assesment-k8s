d87f48137c1767b5ca169b8dc2d455577f4b2269
A.Davarski <42774466+adavarski@users.noreply.github.com>
2024-10-27 14:43:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from waitress import serve
 
 app = Flask(__name__)
 
-
 @app.route("/greet")
 def index():
-    return "<p>Now everyone can be a hero...</p>"
-
+    # Get the 'name' query parameter
+    name = request.args.get("name")
+    if name:
+        # Return JSON response with the name included
+        return jsonify({"message": f"Hello {name}!"})
+    else:
+        # Default message if name is not provided
+        return jsonify({"message": "Now everyone can be a hero..."})
 
 @app.route("/health")
 def health():
-    return ("OK", 200)
-
+    return "OK", 200
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
